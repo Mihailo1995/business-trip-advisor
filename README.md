1ed424e5e5424d4ffc3d84cbe61fcd1ef3602bbc
mihailo1995 <mihailo.ljubenkovic@gmail.com>
2019-08-04 15:44:40 +0200

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Business Trip Advisor
-https://mihailo1995.github.io/business-trip-advisor/ <br />
---
+++ precache-manifest.c0a619527465cadc11bce5f8483589f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e554936fa944946a6f82855d2cfd4218",
+    "url": "/business-trip-advisor/index.html"
+  },
+  {
+    "revision": "2fcb1ef197f4ee63e100",
+    "url": "/business-trip-advisor/static/css/main.9a9856c4.chunk.css"
+  },
+  {
+    "revision": "09be8c504fe918e23497",
+    "url": "/business-trip-advisor/static/js/2.134c189c.chunk.js"
+  },
+  {
+    "revision": "2fcb1ef197f4ee63e100",
+    "url": "/business-trip-advisor/static/js/main.40d37fc3.chunk.js"
+  },
+  {
+    "revision": "627935c76e796d2116d6",
+    "url": "/business-trip-advisor/static/js/runtime~main.b9812d8e.js"
+  },
+  {
+    "revision": "17b828aac4c6210d24e3e3ceaf371e60",
+    "url": "/business-trip-advisor/static/media/home-cover.17b828aa.jpg"
+  },
+  {
+    "revision": "36cd2bf6e24c1cca018617d2a6f4eb58",
+    "url": "/business-trip-advisor/static/media/hotel.36cd2bf6.jpg"
+  },
+  {
+    "revision": "859c4622de182d4bccacd601b6822f76",
+    "url": "/business-trip-advisor/static/media/register-cover.859c4622.jpg"
+  }
+]);
